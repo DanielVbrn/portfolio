0fef50433476280fe5d8e7c0724881a92a33938f
Daniel <danielvbrn@gmail.com>
2024-03-17 15:35:27 -0300

    ...

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -13,7 +13,7 @@ export const SocialNetworks = () => {
   return (
     <section id="social-networks">
         {socialnetworks.map((network) => (
-            <a href="" className="social-btn" id={network.name} key={network.name}>
+            <a href={network.link} className="social-btn" id={network.name} key={network.name}>
                 {network.icon}
             </a>
         ))}
