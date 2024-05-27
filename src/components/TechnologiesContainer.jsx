aebb0fe32eadc61fc95c090e8be07a9e0d1e63c4
Daniel <danielvbrn@gmail.com>
2024-05-27 16:53:27 -0300

    updated resume

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -4,18 +4,60 @@ import {
   DiJsBadge,
   DiNodejsSmall,
   DiPostgresql,
-  DiReact
+  DiReact,
+  DiTypo3,
+  DiGit
 } from "react-icons/di"
 
 import "../styles/components/technologiescontainer.sass"
 
 const technologies = [
-  { id: "html", name: "HTML5", icon: <DiHtml5 /> },
-  { id: "css", name: "CSS3", icon: <DiCss3 /> },
-  { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
-  { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
-  { id: "postgresql", name: "Postgresql", icon: <DiPostgresql /> },
-  { id: "react", name: "React", icon: <DiReact /> },
+  { 
+    id: "html", 
+    name: "HTML5", 
+    icon: <DiHtml5 />, 
+    text:"Já desenvolvi projetos com HTML5 como construção de landing pages e blogs, e tenho contato com essa tecnologia há mais de 2 anos."},
+  { 
+    id: "css", 
+    name: "CSS3", 
+    icon: <DiCss3 />,
+    text:"Já desenvolvi projetos com CSS3 como construção de estilização de páginas, aplicativos e blogs, e tenho contato com essa tecnologia há mais de 2 anos."
+  },
+  { 
+    id: "js", 
+    name: "JavaScript", 
+    icon: <DiJsBadge />,
+    text:"Já desenvolvi projetos com JavaScript, sendo essa a linguagem que aprendi lógica de progração, e tenho contato com essa linguagem a quase 3 anos."
+  },
+  { 
+    id: "node", 
+    name: "Node.js", 
+    icon: <DiNodejsSmall /> ,
+    text:"Já desenvolvi projetos com nodejs, sendo essa a  que aprendi lógica de progração, projetos voltados para o backend de aplicações, como criação de apis para serviços web."
+  },
+  { 
+    id: "postgresql", 
+    name: "Postgresql", 
+    icon: <DiPostgresql />,
+    text:"Já desenvolvi projetos com PostgreSQL, sendo o banco que mais utilizei para projetos, fazendo consultas e executando ações no banco como inserts, updates, deletes."
+
+  },
+  { 
+    id: "react", 
+    name: "React", 
+    icon: <DiReact />,
+    text:"Já desenvolvi projetos em Reactjs, sendo o framework que mais utilizei para frontend com javascript e typescript."
+
+  },
+  { 
+    id: "git", 
+    name: "Git", 
+    icon: <DiGit />,
+    text:"Já desenvolvi projetos com Git para controle de versão, e desenvolvi aplicações junto a minha equipe."
+
+  },
+
+
 ];
 
 const TechnologiesContainer = () => {
@@ -24,18 +66,13 @@ const TechnologiesContainer = () => {
       <h2>Tecnologias</h2>
       <div className="technologies-grid">
         {technologies.map((tech) => (
-            <div className="technology-card" id={tech.id} key={tech.id}>
-              {tech.icon}
+          <div className="technology-card" id={tech.id} key={tech.id}>
               <div className="technology-info">
+                <h2>
+                {tech.icon}
+                </h2>
                 <h3>{tech.name}</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                  Quia praesentium atque quasi aspernatur 
-                  doloremque provident eum dicta molestias 
-                  quisquam, pariatur debitis. Accusamus quia, molestiae a 
-                  facere itaque iure repellat sed!
-                </p>
-              
+                <p>{tech.text}</p>
               </div>
             </div>
           ))}
